ef02a312539b1393c6d7fd039ff862d159521035
dodinhtrung97 <dodinhtrung97@gmail.com>
2018-10-31 13:29:57 +0700

    Upload working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import ReactDropzone from 'react-dropzone';
 import { IoMdCloudUpload } from 'react-icons/io';
 import { IconContext } from 'react-icons';
+import axios from 'axios';
 import './App.css';
+
+const serverUrl = 'http://127.0.0.1:7072/';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
+      bucketName: '',
       fileName: 'Drop or Select a file',
       fileMd5: '',
       fileSize: '',
@@ -15,23 +19,40 @@ class App extends Component {
   }
 
   onDrop = (files) => {
+    console.log(files);
     var fileMd5 = '';
 
     files.forEach(file => {
       const reader = new FileReader();
+
+      reader.onabort = () => console.log('file reading was aborted');
+      reader.onerror = () => console.log('file reading has failed');
       reader.onload = () => {
         var md5 = require('md5');
         const fileAsBinaryString = reader.result;
         fileMd5 = md5(fileAsBinaryString);
 
+        var bucketName = file.name.split(".")[0];
         this.setState({
           fileName: file.name,
-          fileMd5: fileMd5,
-          fileSize: file.size
         })
+
+        // Upload request to bucket
+        // Create bucket with name = fileName
+        axios({
+          method: 'post',
+          url: serverUrl + bucketName + "/" + file.name,
+          data: {
+            fileMd5: fileMd5,
+            fileSize: file.size,
+            data: fileAsBinaryString
+          }
+        }).then( 
+          (res) => console.log(res) 
+        ).catch(
+          console.log("Upload Failed")
+        )
       }
-      reader.onabort = () => console.log('file reading was aborted');
-      reader.onerror = () => console.log('file reading has failed');
 
       try {
         reader.readAsDataURL(file);
@@ -44,7 +65,7 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <ReactDropzone onDrop={e => this.onDrop(e)} className="dropzone">
+        <ReactDropzone onDrop={this.onDrop} className="dropzone">
           <IconContext.Provider value={{ size: "5em" }}>
             <IoMdCloudUpload/>
           </IconContext.Provider>
